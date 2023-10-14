4986002cca9dc78486f5bb3b7ff615029fa2c669
Himanshi Raghav <himanshiraghav9753@gmail.com>
2023-10-14 23:30:36 +0530

    booking and thankyou

---
+++ frontend/src/pages/ThankYou.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Container,Row, Col, Button} from "reactstrap";
+import{Link} from "react-router-dom";
+const Thankyou = () =>{
+    return(
+        <section>
+            <Container>
+                <Row>
+                    <Col lg='12'>
+                    </Col>
+                </Row>
+            </Container>
+        </section>
+    );
+};
+
+export default Thankyou;
