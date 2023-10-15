021d38dbe06833e7be79db53811fb029137584a3
Saksheegithub <saksheeshrivastwa@gmail.com>
2023-10-15 10:38:17 +0530

    changes by sakshi

---
+++ frontend/src/Components/Booking/Image-gallery/galleyimages.js
@@ -0,0 +1,9 @@
+
+
+import galleryImg01 from '../../assets/images/gallery-01.jpg'
+import galleryImg01 from '../../assets/images/gallery-02.jpg'
+import galleryImg01 from '../../assets/images/gallery-03.jpg'
+import galleryImg01 from '../../assets/images/gallery-04.jpg'
+import galleryImg01 from '../../assets/images/gallery-05.jpg'
+import galleryImg01 from '../../assets/images/gallery-06.jpg'
+import galleryImg01 from '../../assets/images/gallery-07.jpg'
